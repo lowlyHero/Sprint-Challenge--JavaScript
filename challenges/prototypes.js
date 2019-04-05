56089c619b4f38ab2ea96d082516519c5d7eeb96
lowlyherotech@gmail.com <[lowlyherotech@gmail.com]>
2019-04-05 12:47:23 -0400

    finished functions.js

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -31,6 +31,7 @@ CuboidMaker.prototype.volume = function () {
 CuboidMaker.prototype.surfaceArea = function () {
     return 2 * (length * width + length * height + width * height);
 }
+
 /* == Step 4: Create a new object that uses CuboidMaker ==
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
